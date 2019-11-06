2adb98dae6f148a4ab43ef3fe425fa71f16282f5
Ben Silverman <bensilverman10@gmail.com>
2019-11-06 20:14:00 +0000

    Main run() function finished

--- dominantColour.js
+++ dominantColour.js
@@ -1,5 +1,6 @@
 let fs = require("fs");
 let decode = require("image-decode");
+let _ = require("underscore");
 
 // Handle individual clusters of pixels.
 class Cluster {
@@ -11,7 +12,7 @@ class Cluster {
         };
     }
 
-    calculateCentroid = function () {
+    calculateCentroid() {
         // Collect all R, G, B values into their own arrays.
         let R = this.pixels.map(pixel => {
             R.push(pixel[0]);
@@ -47,17 +48,39 @@ class kMeans {
         // ...then format it properly.
         this.pixels = formatPixelData(this.pixels);
 
-        // Initialise this.clusters with k "null" elements.
-        this.clusters = [];
+        // Initialise this.clusters as empty with length k.
+        this.clusters = new Array(k);
+
+        // Randomly select k pixels from the pixel data as our starting centroid coordinates.
+        randomCentroids = _.sample(this.pixels, k);
+
         for (let i = 0; i < k; i++) {
-            this.clusters.push(null);
+            this.clusters[i] = new Cluster();
+            this.clusters[i].centroid = randomCentroids[i];
         }
+
+        let iterations = 0
+        while (iterations < this.maxIterations) {
+
+            // Iterate through each pixel and assign it to a cluster.
+            // It is assigned to the cluster with the closest centroid.
+            this.pixels.forEach(pixel => {
+                this.assignCluster(pixel);
+            });
+
+            // Calculate the new centroid for each cluster.
+            this.clusters.forEach(cluster => {
+                this.calculateCentroid();
+            });
+
+            iterations++;
+        }
+
+
     }
 }
 
 // Converts the pixel data returned by "decode-image" from Uint8Array to a 2D array: [[r,g,b], [r,g,b] ...]
-/* FIXME: This is not going to be correct because I also need a way of storing the coordinates of each pixel
-          otherwise you can't calcualte the distance of each to its nearest centroid. */
 function formatPixelData(pixels) {
     let output = [];
     let pixelData = Array.from(pixels.data);
@@ -65,8 +88,8 @@ function formatPixelData(pixels) {
     console.log(pixelData);
 
     for (let i = 0; i < pixelData.length; i = i + 4) {
-        console.log(pixelData.slice(i,i+3));
-        output.push(pixelData.slice(i,i+3));
+        console.log(pixelData.slice(i, i + 3));
+        output.push(pixelData.slice(i, i + 3));
     }
 
     return output;
